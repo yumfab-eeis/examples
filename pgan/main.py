8be6905c73d6c64cbfacb83ad51e5b342d4883c9
yuma <fableooon@gmail.com>
2018-08-23 16:41:30 +0900

    remove print

--- pgan/main.py
+++ pgan/main.py
@@ -116,7 +116,6 @@ class NoiseTranpose2d(nn.Module):
     def forward(self, x):
         tmp1 = x.data.shape
         tmp2 = self.noise.shape
-        print (tmp1, tmp2)
         if (tmp1[1] != tmp2[1]) or (tmp1[2] != tmp2[2]) or (tmp1[3] != tmp2[3]):
             print ("                             ho!!!!!!!!!!!!!!!")
             self.noise = (2*torch.rand(x.data.shape)-1)*self.level
