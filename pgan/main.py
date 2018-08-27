12f4015caf10adcd636b07b3c383e3589d9a692c
yuma <fableooon@gmail.com>
2018-08-27 14:57:54 +0900

    change Adam

--- pgan/main.py
+++ pgan/main.py
@@ -264,7 +264,7 @@ real_label = 1
 fake_label = 0
 
 # setup optimizer
-optimizerD = optim.Adam(netD.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
+optimizerD = optim.Adam(filter(lambda p: p.requires_grad,netD.parameters()), lr=opt.lr, betas=(opt.beta1, 0.999))
 optimizerG = optim.Adam(netG.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
 
 for epoch in range(opt.niter):
