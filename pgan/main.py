dae828860b05f467ae941a7db367d3f4c90777d9
yuma <fableooon@gmail.com>
2018-08-27 15:41:14 +0900

    no message

--- pgan/main.py
+++ pgan/main.py
@@ -280,7 +280,8 @@ for epoch in range(opt.niter):
         label = torch.full((batch_size,), real_label, device=device)
 
         output = netD(real_cpu)
-        print (output.size())
+        if output < 0:
+            print ('0!!!')
         errD_real = criterion(output, label)
         errD_real.backward()
         D_x = output.mean().item()
