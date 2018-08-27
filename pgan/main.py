6769f6ec41da213201f0c57296fc7f6cdad7e9cd
yuma <fableooon@gmail.com>
2018-08-27 14:34:11 +0900

    add conventional generator

--- pgan/main.py
+++ pgan/main.py
@@ -137,6 +137,40 @@ class Generator(nn.Module):
         self.ngpu = ngpu
         self.main = nn.Sequential(
             # input is Z, going into a convolution
+            nn.ConvTranspose2d(     nz, ngf * 8, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(ngf * 8),
+            nn.ReLU(True),
+            # state size. (ngf*8) x 4 x 4
+            nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 4),
+            nn.ReLU(True),
+            # state size. (ngf*4) x 8 x 8
+            nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 2),
+            nn.ReLU(True),
+            # state size. (ngf*2) x 16 x 16
+            nn.ConvTranspose2d(ngf * 2,     ngf, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf),
+            nn.ReLU(True),
+            # state size. (ngf) x 32 x 32
+            nn.ConvTranspose2d(    ngf,      nc, 4, 2, 1, bias=False),
+            nn.Tanh()
+            # state size. (nc) x 64 x 64
+        )
+
+    def forward(self, input):
+        if input.is_cuda and self.ngpu > 1:
+            output = nn.parallel.data_parallel(self.main, input, range(self.ngpu))
+        else:
+            output = self.main(input)
+        return output
+
+class P_Generator(nn.Module):
+    def __init__(self, ngpu):
+        super(Generator, self).__init__()
+        self.ngpu = ngpu
+        self.main = nn.Sequential(
+            # input is Z, going into a convolution
             nn.Upsample(scale_factor=2, mode='nearest'),
             NoiseTranpose2d(     nz, ngf * 8, 0.1),
             NoiseTranpose2d(     ngf * 8, ngf * 8, 0.1),
@@ -161,7 +195,6 @@ class Generator(nn.Module):
             NoiseTranpose2d(    ngf,      ngf, 0.1),
             NoiseTranpose2d(    ngf,      ngf, 0.1),
             NoiseTranpose2d(    ngf,      nc, 0.1),
-            NoiseTranpose2d(     nc,      nc, 0.1),
             nn.Tanh()
             # state size. (nc) x 64 x 64
         )
@@ -232,7 +265,7 @@ fake_label = 0
 optimizerD = optim.Adam(netD.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
 optimizerG = optim.Adam(netG.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
 
-update_rate = 2
+update_rate = 1
 
 for epoch in range(opt.niter):
     for i, data in enumerate(dataloader, 0):
@@ -285,40 +318,6 @@ for epoch in range(opt.niter):
                 vutils.save_image(fake.detach(),
                         '%s/fake_samples_epoch_%03d.png' % (opt.outf, epoch),
                         normalize=True)
-        #
-        # # train with fake
-        # noise = torch.randn(batch_size, nz, 1, 1, device=device)
-        # fake = netG(noise)
-        # label.fill_(fake_label)
-        # output = netD(fake.detach())
-        # errD_fake = criterion(output, label)
-        # errD_fake.backward()
-        # D_G_z1 = output.mean().item()
-        # errD = errD_real + errD_fake
-        # optimizerD.step()
-        #
-        # ############################
-        # # (2) Update G network: maximize log(D(G(z)))
-        # ###########################
-        # netG.zero_grad()
-        # label.fill_(real_label)  # fake labels are real for generator cost
-        # output = netD(fake)
-        # errG = criterion(output, label)
-        # errG.backward()
-        # D_G_z2 = output.mean().item()
-        # optimizerG.step()
-        #
-        # print('[%d/%d][%d/%d] Loss_D: %.4f Loss_G: %.4f D(x): %.4f D(G(z)): %.4f / %.4f'
-        #       % (epoch, opt.niter, i, len(dataloader),
-        #          errD.item(), errG.item(), D_x, D_G_z1, D_G_z2))
-        # if i % 100 == 0:
-        #     vutils.save_image(real_cpu,
-        #             '%s/real_samples.png' % opt.outf,
-        #             normalize=True)
-        #     fake = netG(fixed_noise)
-        #     vutils.save_image(fake.detach(),
-        #             '%s/fake_samples_epoch_%03d.png' % (opt.outf, epoch),
-        #             normalize=True)
 
     # do checkpointing
     torch.save(netG.state_dict(), '%s/netG_epoch_%d.pth' % (opt.outf, epoch))
