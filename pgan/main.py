a57613257031f3618d26675fa8f74adb71df6130
yuma <fableooon@gmail.com>
2018-09-10 15:29:47 +0900

    fix opt

--- pgan/main.py
+++ pgan/main.py
@@ -128,10 +128,15 @@ real_label = 1
 fake_label = 0
 
 # setup optimizer
-optimizerD = optim.Adam(netD.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
-optimizerG = optim.Adam(netG.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
-# optimizerD = optim.Adam(filter(lambda p: p.requires_grad,netD.parameters()), lr=opt.lr, betas=(opt.beta1, 0.999))
-# optimizerG = optim.Adam(filter(lambda p: p.requires_grad,netD.parameters()), lr=opt.lr, betas=(opt.beta1, 0.999))
+if opt.activatePG:
+    optimizerG = optim.Adam(filter(lambda p: p.requires_grad,netG.parameters()), lr=opt.lr, betas=(opt.beta1, 0.999))
+else:
+    optimizerG = optim.Adam(netG.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
+
+if opt.activatePD:
+    optimizerD = optim.Adam(filter(lambda p: p.requires_grad,netD.parameters()), lr=opt.lr, betas=(opt.beta1, 0.999))
+else:
+    optimizerD = optim.Adam(netD.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
 
 for epoch in range(opt.niter):
     for i, data in enumerate(dataloader, 0):
