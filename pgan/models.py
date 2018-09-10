470be2c4adaa99d3fb71544c11bc199afb76152d
yuma <fableooon@gmail.com>
2018-09-10 16:38:57 +0900

    no message

--- pgan/models.py
+++ pgan/models.py
@@ -399,7 +399,7 @@ def noiseresgenerator76(nchannels, nfilters, nclasses, level=0.1):
     return NoiseResGenetator(NoiseNoPoolBlock, [3,4,6,8,10,12,3], nchannels=nchannels, nfilters=nfilters, nclasses=nclasses, level=level)
 
 def noiseresgenerator101(nchannels, nfilters, nclasses, level=0.1):
-    return NoiseResGenetator(NoiseNoPoolBlock, [3,4,4,8,12,24,3], nchannels=nchannels, nfilters=nfilters, nclasses=nclasses, level=level)
+    return NoiseResGenetator(NoiseNoPoolBlock, [3,4,4,8,12,20,3], nchannels=nchannels, nfilters=nfilters, nclasses=nclasses, level=level)
 
 def noiseresgenerator152(nchannels, nfilters, nclasses, level=0.1):
     return NoiseResGenetator(NoiseNoPoolBlock, [3,8,36,3], nchannels=nchannels, nfilters=nfilters, nclasses=nclasses, level=level)
