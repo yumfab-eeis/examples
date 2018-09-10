b1b1fc284c0819c8c2482d6ab6b07f58df173ce9
yuma <fableooon@gmail.com>
2018-09-10 15:45:13 +0900

    no message

--- pgan/models.py
+++ pgan/models.py
@@ -335,7 +335,7 @@ class NoiseResGenetator(nn.Module):
         super(NoiseResGenetator, self).__init__()
         self.in_planes = nfilters
         self.layer1 = self._make_layer(block, 8*nfilters, nblocks[0], level=level)
-        self.layer2 = self._make_layer(block, 4*nfilters, nblocks[1], level=level)
+        self.layer2 = self._make_layer(block, 8*nfilters, nblocks[1], level=level)
         self.layer3 = self._make_layer(block, 4*nfilters, nblocks[2], level=level)
         self.layer4 = self._make_layer(block, 2*nfilters, nblocks[3], level=level)
         self.layer5 = self._make_layer(block, 2*nfilters, nblocks[4], level=level)
