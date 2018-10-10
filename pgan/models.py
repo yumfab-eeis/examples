97809a77dc0242813b1a782018b21b3a4dc501a5
yuma <fableooon@gmail.com>
2018-10-10 19:50:01 +0900

    no message

--- pgan/models.py
+++ pgan/models.py
@@ -339,8 +339,8 @@ class NoiseResGenetator(nn.Module):
             nn.BatchNorm2d(128*nfilters),
             nn.ReLU(True),
         )
-        self.layer1 = self._make_layer(block, 128*nfilters, 32*nfilters, nblocks[0], level=level)
-        self.layer2 = self._make_layer(block, 32*nfilters, 8*nfilters, nblocks[1], level=level)
+        self.layer1 = self._make_layer(block, 128*nfilters, 16*nfilters, nblocks[0], level=level)
+        self.layer2 = self._make_layer(block, 16*nfilters, 8*nfilters, nblocks[1], level=level)
         self.layer3 = self._make_layer(block, 8*nfilters, 4*nfilters, nblocks[2], level=level)
         self.layer4 = self._make_layer(block, 4*nfilters, 1*nfilters, nblocks[3], level=level)
         self.layer5 = self._make_layer(block, 1*nfilters, nchannels, nblocks[4], level=level, isLastBN=False)
