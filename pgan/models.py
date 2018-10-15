ddfb475ba65ab50f47cef41e9ee0e681b2abf8f9
yuma <fableooon@gmail.com>
2018-10-15 17:42:11 +0900

    no message

--- pgan/models.py
+++ pgan/models.py
@@ -321,9 +321,9 @@ class NoiseGenetator(nn.Module):
 
     def _make_layer(self, block, in_planes, out_planes, nblocks, stride=1, level=0.2):
         layers = []
-        layers.append(block(in_planes, planes, stride, shortcut, level=level))
+        #layers.append(block(in_planes, out_planes, stride, shortcut, level=level))
         for i in range(1, nblocks):
-            layers.append(block(in_planes, planes, level=level))
+            layers.append(block(in_planes, out_planes, level=level))
         return nn.Sequential(*layers)
 
     def forward(self, x):
