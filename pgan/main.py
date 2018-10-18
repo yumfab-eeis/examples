32ad14025fbbedb3b742e535953dfc581aecd3ae
yuma <fableooon@gmail.com>
2018-10-18 17:18:11 +0900

    add WGAN

--- pgan/main.py
+++ pgan/main.py
@@ -40,6 +40,11 @@ parser.add_argument('--trainRateG', type=int, default=1, help='train rate of G:D
 parser.add_argument('--trainRateD', type=int, default=1, help='train rate of D:G')
 parser.add_argument('--activatePG', action='store_true', help='activate Generator with PNN')
 parser.add_argument('--activatePD', action='store_true', help='activate Discriminator with PNN')
+parser.add_argument('--Diters', type=int, default=5, help='number of D iters per each G iter')
+parser.add_argument('--clamp_lower', type=float, default=-0.01)
+parser.add_argument('--clamp_upper', type=float, default=0.01)
+parser.add_argument('--activateWGAN', action='store_true', help='activate WassersteinGAN')
+parser.add_argument('--adam', action='store_true', help='Whether to use adam (default is rmsprop)')
 
 opt = parser.parse_args()
 print(opt)
@@ -143,68 +148,161 @@ fake_label = 0
 #print ('update parameters:',list(filter(lambda p: p.requires_grad,netG.parameters())))
 
 # setup optimizer
-if opt.activatePG:
-    optimizerG = optim.Adam(filter(lambda p: p.requires_grad,netG.parameters()), lr=opt.lrG, betas=(opt.beta1, 0.999), weight_decay=1e-8)
+if opt.adam:
+    if opt.activatePG:
+        optimizerG = optim.Adam(filter(lambda p: p.requires_grad,netG.parameters()), lr=opt.lrG, betas=(opt.beta1, 0.999), weight_decay=1e-8)
+    else:
+        optimizerG = optim.Adam(netG.parameters(), lr=opt.lrG, betas=(opt.beta1, 0.999), weight_decay=1e-8)
+
+    if opt.activatePD:
+        optimizerD = optim.Adam(filter(lambda p: p.requires_grad,netD.parameters()), lr=opt.lrD, betas=(opt.beta1, 0.999), weight_decay=1e-8)
+    else:
+        optimizerD = optim.Adam(netD.parameters(), lr=opt.lrD, betas=(opt.beta1, 0.999), weight_decay=1e-8)
 else:
-    optimizerG = optim.Adam(netG.parameters(), lr=opt.lrG, betas=(opt.beta1, 0.999), weight_decay=1e-8)
+    if opt.activatePG:
+        optimizerG = optim.RMSprop(filter(lambda p: p.requires_grad,netG.parameters()), lr=opt.lrG)
+    else:
+        optimizerG = optim.RMSprop(netG.parameters(), lr=opt.lrG)
 
-if opt.activatePD:
-    optimizerD = optim.Adam(filter(lambda p: p.requires_grad,netD.parameters()), lr=opt.lrD, betas=(opt.beta1, 0.999), weight_decay=1e-8)
-else:
-    optimizerD = optim.Adam(netD.parameters(), lr=opt.lrD, betas=(opt.beta1, 0.999), weight_decay=1e-8)
+    if opt.activatePD:
+        optimizerD = optim.RMSprop(filter(lambda p: p.requires_grad,netD.parameters()), lr=opt.lrD)
+    else:
+        optimizerD = optim.RMSprop(netD.parameters(), lr=opt.lrD)
 
-for epoch in range(opt.niter):
-    for i, data in enumerate(dataloader, 0):
-        ############################
-        # (1) Update D network: maximize log(D(x)) + log(1 - D(G(z)))
-        ###########################
-        for j in range(opt.trainRateD):
-            # train with real
-            netD.zero_grad()
-            real_cpu = data[0].to(device)
-            batch_size = real_cpu.size(0)
-            #batch_size = opt.batchSize
-            label = torch.full((batch_size,), real_label, device=device)
+# START TRAINING
+if opt.activateWGAN:
+    # ---WGAN Training---
+    gen_iterations = 0
+    for epoch in range(opt.niter):
+        data_iter = iter(dataloader)
+        i = 0
+        while i < len(dataloader):
+            ############################
+            # (1) Update D network
+            ###########################
+            for p in netD.parameters(): # reset requires_grad
+                p.requires_grad = True # they are set to False below in netG update
 
-            output = netD(real_cpu)
-            errD_real = criterion(output, label)
-            errD_real.backward()
-            D_x = output.mean().item()
+            # train the discriminator Diters times
+            if gen_iterations < 25 or gen_iterations % 500 == 0:
+                Diters = 100
+            else:
+                Diters = opt.Diters
+            j = 0
+            while j < Diters and i < len(dataloader):
+                j += 1
 
-        for j in range(opt.trainRateG):
-            # train with fake
-            noise = torch.randn(batch_size, nz, 1, 1, device=device)
-            fake = netG(noise)
-            label.fill_(fake_label)
-            output = netD(fake.detach())
-            errD_fake = criterion(output, label)
-            errD_fake.backward()
-            D_G_z1 = output.mean().item()
-            errD = errD_real + errD_fake
-            optimizerD.step()
+                # clamp parameters to a cube
+                for p in netD.parameters():
+                    p.data.clamp_(opt.clamp_lower, opt.clamp_upper)
+
+                data = data_iter.next()
+                i += 1
+
+                # train with real
+                real_cpu, _ = data
+                netD.zero_grad()
+                batch_size = real_cpu.size(0)
+
+                if opt.cuda:
+                    real_cpu = real_cpu.cuda()
+                input.resize_as_(real_cpu).copy_(real_cpu)
+                inputv = Variable(input)
+
+                errD_real = netD(inputv)
+                errD_real.backward(one)
+
+                # train with fake
+                noise.resize_(opt.batchSize, nz, 1, 1).normal_(0, 1)
+                noisev = Variable(noise, volatile = True) # totally freeze netG
+                fake = Variable(netG(noisev).data)
+                inputv = fake
+                errD_fake = netD(inputv)
+                errD_fake.backward(mone)
+                errD = errD_real - errD_fake
+                optimizerD.step()
 
             ############################
-            # (2) Update G network: maximize log(D(G(z)))
+            # (2) Update G network
             ###########################
+            for p in netD.parameters():
+                p.requires_grad = False # to avoid computation
             netG.zero_grad()
-            label.fill_(real_label)  # fake labels are real for generator cost
-            output = netD(fake)
-            errG = criterion(output, label)
-            errG.backward()
-            D_G_z2 = output.mean().item()
+            # in case our last batch was the tail batch of the dataloader,
+            # make sure we feed a full batch of noise
+            noise.resize_(opt.batchSize, nz, 1, 1).normal_(0, 1)
+            noisev = Variable(noise)
+            fake = netG(noisev)
+            errG = netD(fake)
+            errG.backward(one)
             optimizerG.step()
+            gen_iterations += 1
 
-            print('[%d/%d][%d/%d] Loss_D: %.4f Loss_G: %.4f D(x): %.4f D(G(z)): %.4f / %.4f'
-                  % (epoch, opt.niter, i, len(dataloader),
-                     errD.item(), errG.item(), D_x, D_G_z1, D_G_z2))
-            if i % 100 == 0:
-                vutils.save_image(real_cpu,
-                        '%s/real_samples.png' % opt.outf,
-                        normalize=True)
-                fake = netG(fixed_noise)
-                vutils.save_image(fake.detach(),
-                        '%s/fake_samples_epoch_%03d_%04d.png' % (opt.outf, epoch, i),
-                        normalize=True)
+            print('[%d/%d][%d/%d][%d] Loss_D: %f Loss_G: %f Loss_D_real: %f Loss_D_fake %f'
+                % (epoch, opt.niter, i, len(dataloader), gen_iterations,
+                errD.data[0], errG.data[0], errD_real.data[0], errD_fake.data[0]))
+            if gen_iterations % 500 == 0:
+                real_cpu = real_cpu.mul(0.5).add(0.5)
+                vutils.save_image(real_cpu, '{0}/real_samples.png'.format(opt.outf))
+                fake = netG(Variable(fixed_noise, volatile=True))
+                fake.data = fake.data.mul(0.5).add(0.5)
+                vutils.save_image(fake.data, '{0}/fake_samples_{1}.png'.format(opt.outf, gen_iterations))
+    # ---WGAN Training End---
+else:
+    # ---DCGAN Training---
+    for epoch in range(opt.niter):
+        for i, data in enumerate(dataloader, 0):
+            ############################
+            # (1) Update D network: maximize log(D(x)) + log(1 - D(G(z)))
+            ###########################
+            for j in range(opt.trainRateD):
+                # train with real
+                netD.zero_grad()
+                real_cpu = data[0].to(device)
+                batch_size = real_cpu.size(0)
+                #batch_size = opt.batchSize
+                label = torch.full((batch_size,), real_label, device=device)
+
+                output = netD(real_cpu)
+                errD_real = criterion(output, label)
+                errD_real.backward()
+                D_x = output.mean().item()
+
+            for j in range(opt.trainRateG):
+                # train with fake
+                noise = torch.randn(batch_size, nz, 1, 1, device=device)
+                fake = netG(noise)
+                label.fill_(fake_label)
+                output = netD(fake.detach())
+                errD_fake = criterion(output, label)
+                errD_fake.backward()
+                D_G_z1 = output.mean().item()
+                errD = errD_real + errD_fake
+                optimizerD.step()
+
+                ############################
+                # (2) Update G network: maximize log(D(G(z)))
+                ###########################
+                netG.zero_grad()
+                label.fill_(real_label)  # fake labels are real for generator cost
+                output = netD(fake)
+                errG = criterion(output, label)
+                errG.backward()
+                D_G_z2 = output.mean().item()
+                optimizerG.step()
+
+                print('[%d/%d][%d/%d] Loss_D: %.4f Loss_G: %.4f D(x): %.4f D(G(z)): %.4f / %.4f'
+                      % (epoch, opt.niter, i, len(dataloader),
+                         errD.item(), errG.item(), D_x, D_G_z1, D_G_z2))
+                if i % 100 == 0:
+                    vutils.save_image(real_cpu,
+                            '%s/real_samples.png' % opt.outf,
+                            normalize=True)
+                    fake = netG(fixed_noise)
+                    vutils.save_image(fake.detach(),
+                            '%s/fake_samples_epoch_%03d_%04d.png' % (opt.outf, epoch, i),
+                            normalize=True)
+    # ---DCGAN Training End---
 
     # do checkpointing
     torch.save(netG.state_dict(), '%s/netG_epoch_%d.pth' % (opt.outf, epoch))
