4eaa325cd08a59a01f75acae81262139dd89a7a2
yuma <fableooon@gmail.com>
2018-10-22 16:49:14 +0900

    import _pickle as cPickle

--- wgan-gp/tflib/mnist.py
+++ wgan-gp/tflib/mnist.py
@@ -3,7 +3,8 @@ import numpy
 import os
 import urllib
 import gzip
-import cPickle as pickle
+#import cPickle as pickle
+import _pickle as cPickle
 
 def mnist_generator(data, batch_size, n_labelled, limit=None):
     images, targets = data
