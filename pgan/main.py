5a1784fc1f629a337619a8f4e3bf7443d0ae9e8d
yuma <fableooon@gmail.com>
2018-10-26 12:01:37 +0900

    no message

--- pgan/main.py
+++ pgan/main.py
@@ -258,12 +258,11 @@ if opt.activateWGAN:
             errG.backward(one)
             optimizerG.step()
             gen_iterations += 1
-            print (gen_iterations)
 
             print('[%d/%d][%d/%d][%d] Loss_D: %f Loss_G: %f Loss_D_real: %f Loss_D_fake %f'
                 % (epoch, opt.niter, i, len(dataloader), gen_iterations,
                 errD.data[0], errG.data[0], errD_real.data[0], errD_fake.data[0]))
-            if gen_iterations % 500 == 0:
+            if gen_iterations % 1 == 0:
                 real_cpu = real_cpu.mul(0.5).add(0.5)
                 vutils.save_image(real_cpu, '{0}/real_samples.png'.format(opt.outf))
                 fake = netG(Variable(fixed_noise, volatile=True))
