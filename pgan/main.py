f141a7573fa2e23a059a6210c661b881058e963e
yuma <fableooon@gmail.com>
2018-10-26 11:32:01 +0900

    no message

--- pgan/main.py
+++ pgan/main.py
@@ -220,14 +220,14 @@ if opt.activateWGAN:
 
                 if opt.cuda:
                     real_cpu = real_cpu.cuda()
-                print (real_cpu.size())
-                input.resize_as_(real_cpu).copy_(real_cpu)
+                #print (real_cpu.size())
+                #input.resize_as_(real_cpu).copy_(real_cpu)
                 inputv = Variable(input)
 
                 errD_real = netD(inputv)
                 #errD_real.backward(one)
                 errD_real.backward(one[0:inputv.size()[0]])
-                print (one[0:inputv.size()[0]].size())
+                #print (one[0:inputv.size()[0]].size())
 
                 # train with fake
                 noise.resize_(opt.batchSize, nz, 1, 1).normal_(0, 1)
@@ -237,7 +237,7 @@ if opt.activateWGAN:
                 errD_fake = netD(inputv)
                 #errD_fake.backward(mone)
                 errD_fake.backward(mone[0:inputv.size()[0]])
-                print (mone[0:inputv.size()[0]].size())
+                #print (mone[0:inputv.size()[0]].size())
 
                 errD = errD_real - errD_fake
                 optimizerD.step()
