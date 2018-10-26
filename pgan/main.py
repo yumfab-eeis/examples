611659a754f08dfecde8d4b83623e92f1748cf94
yuma <fableooon@gmail.com>
2018-10-26 11:28:04 +0900

    no message

--- pgan/main.py
+++ pgan/main.py
@@ -220,7 +220,7 @@ if opt.activateWGAN:
 
                 if opt.cuda:
                     real_cpu = real_cpu.cuda()
-                print (real_cpu)
+                print (real_cpu.size())
                 input.resize_as_(real_cpu).copy_(real_cpu)
                 inputv = Variable(input)
 
