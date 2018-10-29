f9cfebe77799bf3c38dae1e4326c5dd6c9991faa
yuma <fableooon@gmail.com>
2018-10-29 18:17:28 +0900

    no message

--- pgan/main.py
+++ pgan/main.py
@@ -286,7 +286,7 @@ if opt.wGAN:
             print('[%d/%d][%d/%d][%d] Loss_D: %f Loss_G: %f Loss_D_real: %f Loss_D_fake %f'
                 % (epoch, opt.niter, i, len(dataloader), gen_iterations,
                 errD.data[0], errG.data[0], errD_real.data[0], errD_fake.data[0]))
-            if gen_iterations % 2 == 0:
+            if gen_iterations % 1 == 0:
                 real_cpu = real_cpu.mul(0.5).add(0.5)
                 vutils.save_image(real_cpu, '{0}/real_samples.png'.format(opt.outDict))
                 with torch.no_grad():
